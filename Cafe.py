e6df5ff73a87437de1b560cc47c6a374366d88b8
Cameron Morton <cameron.morton@accenture.com>
2025-03-11 13:43:01 +0000

    Added virtual and new order
    Ditto

--- Cafe.py
+++ Cafe.py
@@ -1,3 +1,4 @@
+import secrets
 menu_dict = {}
 def CreateNewLocalOrder(**order):
     order_list =[]
@@ -9,12 +10,14 @@ def CreateNewLocalOrder(**order):
     return order_list, order_cost
 
 def MoveOrderToFile():
+    print("Moving Order")
     newOrder = CreateNewLocalOrder(Coffee = 3, Cola = 2)
     with open('Orders.txt','r+') as file:
         lines = file.readlines()
-        lines.append(newOrder)
+        lines.append(str(newOrder) + "ID:" + secrets.token_hex(16))
     with open("Orders.txt", "w") as file:
-        file.writelines(str(lines) + ":Order Status = Created;")    
+        file.writelines(str(lines) + ":Order Status = Created;" + '\n') 
+    print("ORDER-MOVED")   
 
 def DisplayGreetingMessage():
     print("Hello and welcome to the Cafe!")
@@ -23,6 +26,9 @@ def DisplayGreetingMessage():
 def AddItem(item_type,item_name,item_cost):
     with open('Menu.txt','r+') as file:
         lines = file.readlines()
+        for line in lines:
+            if(line.find(item_name)):
+                print("ITEM ALREADY IN")
         linNo = 0
         match item_type:
             case "SNACK":
@@ -43,12 +49,13 @@ def AddItem(item_type,item_name,item_cost):
         file.writelines(lines)
     GetMenu()
 
-def SetUpLocalOrder(**order):
+def SetUpLocalOrder():
+    GetMenu()
     DisplayMenu()
     responce = input("Please take your time and view our menu! Let us know what you would like : )")
     if(menu_dict.get(responce)):
         No = input((f"How many {responce} would you like to add?"))
-    CreateNewLocalOrder(responce = Nogithu)
+    CreateNewLocalOrder(responce,No)
     
 
 
@@ -88,19 +95,33 @@ def DisplayMenu():
     return
 
 def ManageProgramFlow():
+    inMenu = True
+
     GetMenu()
+    MoveOrderToFile()
     DisplayGreetingMessage()
-    responce = input(" 1. Place a new order \n 2. Create a new menu item \n 3. Check on a current order")
-    match responce:
-        case "1":
-            print("1")
-            SetUpLocalOrder()
-        case "2":
-            print("2")
-        case "3":
-            print("3")
-        case _:
-            print("Please enter your resonse as a single digit")
+    while inMenu:
+        responce = input(" 1. Place a new order \n 2. Create a new menu item \n 3. Check on a current order")
+        match responce:
+            case "1":
+                print("1")
+                SetUpLocalOrder()
+            case "2":
+                print("2")
+                item_name = input("What is the name of the item you would like to add?")
+                item_cost = input("What is the price in p of the itme you would like to add?")
+                item_type = input("What type of item; S = SNACK, D = DRINK")
+                if(item_type == 'S'):
+                    item_type = "SNACK"
+                elif(item_type =='D'):
+                    item_type = "DRINK" 
+                else:
+                    break   
+                AddItem(item_name = item_name,item_cost= item_cost,item_type = item_type)
+            case "3":
+                print("3")
+            case _:
+                print("Please enter your resonse as a single digit")
     return
 """ print(current_Item, " : " ,'£' , "{:.2f}".format(round((float(current_Price) / 100),2)))"""                     
 
